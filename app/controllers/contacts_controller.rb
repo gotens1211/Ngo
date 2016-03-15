5fce65b7786ef011dce42c7e984348ca02932dfb
gotens1211 <nikeythe@gmail.com>
2016-03-15 16:46:26 +0530

    Created show and destroy actions for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,6 +20,12 @@ class ContactsController < ApplicationController
 	def show
 		@contact = Contact.find(params[:id])
 	end
+	def destroy
+		@contact = Contact.find(params[:id])
+		@contact.destroy
+		flash[:success] = "Message Successfully Deleted."
+		redirect_to contacts_path
+	end
 	private
 	def contact_params
 		params.require(:contact).permit(:name,:email_id,:phone,:subject,:requirement)
